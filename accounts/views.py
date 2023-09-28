2980120f19b19fec422fccaf35073000b14f9260
Dib <93922964+dibXpert@users.noreply.github.com>
2023-09-28 11:37:51 +0800

    url routing and views setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('home')
+
+def items(request):
+    return HttpResponse('items')
+
+def staff(request):
+    return HttpResponse('staff')
