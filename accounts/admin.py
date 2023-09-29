46e76b99db76614d76279c1c2163a1c0c3a7f7a9
Dib <93922964+dibXpert@users.noreply.github.com>
2023-09-30 02:04:58 +0800

    tables

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Staff)
+admin.site.register(Item)
+admin.site.register(History)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Staff(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    STATUS = (
+        ('Available', 'Available'),
+        ('Empty', 'Empty'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    amount = models.IntegerField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+class History(models.Model): 
+    STATUS = (
+        ('Borrowed','Borrowed'),
+        ('Returned','Returned')
+    )
+    #staff =
+    #item =
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
