38dc66d6aeba664a65fafcf45c8a157f41dd3a63
Dib <93922964+dibXpert@users.noreply.github.com>
2023-10-03 00:42:00 +0800

    scroll bar datatables

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
-    
-    
+    path('logout/', views.logoutUser, name="logout"),
+
     
     path('', views.home, name="home"),
     path('items/', views.items, name="items"),
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,10 @@ from django.http import HttpResponse
 
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
 from .models import *
 from .forms import BorrowedForm, CreateUserForm
 from .filters import BorrowFilter
@@ -16,6 +20,9 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            
             return redirect('login')
     
     
@@ -24,9 +31,26 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+    
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+      #make sure user is in the database  
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'username OR password is incorrect')        
+        
     context ={}
     return render(request, 'accounts/login.html', context)
 
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 def home(request):
